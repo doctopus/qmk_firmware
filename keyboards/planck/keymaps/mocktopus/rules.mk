725149fdecccba325cbc3bff82011fae7eb41fd2
Doctopus <tapasapt@gmail.com>
2020-05-03 01:12:28 -0400

    Mocktopus modification

---
+++ keyboards/planck/keymaps/mocktopus/rules.mk
@@ -0,0 +1,6 @@
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+AUDIO_ENABLE = yes
+
+#DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
+# As said in https://github.com/qmk/qmk_firmware/blob/master/docs/flashing.md so that your device resets after flashing
